79b60e2026171d8353e75ad810dc600b20daa1f0
Julia Ricketti <julsricketti@gmail.com>
2017-02-11 14:44:00 -0500

    Implemented the mechanism to update the cities select box according to the country selected

--- js/panel.js
+++ js/panel.js
@@ -15,6 +15,10 @@ export default class PlaceAndParameter extends Component{
             parameters:[],
             error: false
         };
+        
+        // we need to use the bind function so we can later have the same scope of the
+        // this object as now, in order to access the state.
+        this.onSelectChangeCountry = this.onSelectChangeCountry.bind(this);
     }
     
     componentWillMount(){
@@ -22,23 +26,36 @@ export default class PlaceAndParameter extends Component{
         
         axios.get("https://api.openaq.org/v1/countries")
         .then(function(response){
-            console.log ("results: ", response.data.results);
             _this.setState({countries: response.data.results})
         });
     }
     
+    onSelectChangeCountry(e){
+     console.log('hiii', e.target.value);
+        var _this = this;
+        //Objective: when the country changes, we need to update the values in the city select box.
+        //For this, we first start with a get request to list the cities of the selected country:
+        axios.get("https://api.openaq.org/v1/cities?limit=1000&country="+e.target.value)
+        .then(function(response){
+            console.log('response:', response);
+            _this.setState({cities: response.data.results});
+        });
+        
+    }
+    
     render(){
+        // TODO: fix the format of the strings so with subscript
        var parameters=["PM 2.5", "PM 10", "SO2", "NO2", "O3", "CO", "BC"];
        return (
            <Panel bsStyle="primary" header="Place & Parameter">
                 <SelectForm controlId="countriesId" controlLabel="Country" things="countries" property="name"
-                options={this.state.countries}/>
+                options={this.state.countries} onChange={this.onSelectChangeCountry}/>
                 <SelectForm controlId="citiesId" controlLabel="City" things="cities" property="city"
-                options={this.state.cities}/>
+                options={this.state.cities} onChange={this.onSelectChangeCountry}/>
                 <SelectForm controlId="locationsId" controlLabel="Location" things="locations" property="location"
-                options={this.state.locations}/>
+                options={this.state.locations} onChange={this.onSelectChangeCountry}/>
                 <SelectForm controlId="parametersId" controlLabel="Parameter" things="parameters" property=""
-                options={parameters}/>
+                options={parameters} onChange={this.onSelectChangeCountry}/>
            </Panel>
        ); 
     }
@@ -47,24 +64,28 @@ export default class PlaceAndParameter extends Component{
 class SelectForm extends Component{
     
     render(){
-     // TODO: fix the options rendering           
-        console.log ("options:", this.props.options);
+     // TODO: fix the options rendering
         var _this = this;
         
         return (
             <FormGroup controlId={this.props.controlId}>
                 <ControlLabel>{this.props.controlLabel}</ControlLabel>
-                <FormControl componentClass="select" placeholder={"All "+this.props.things}>
+                <FormControl componentClass="select" placeholder={"All "+this.props.things}
+                    onChange={this.props.onChange}>
                     <option>{"All "+this.props.things}</option>
                     {
-                      this.props.options.map(function(value, key) {
+                      this.props.options.map(function(option, key) {
                         /*the way this has been implemented, the parameters options array will not
                         have a property, so we need to check that before rendering the options, using the
                         props.
                         */
-                        if(_this.props.property.length>0)
-                            value = value[_this.props.property]
-                      return <option key={key}>{value}</option>
+                        var code = {}; 
+                        if(_this.props.property.length>0){
+                            code = option.code; //we need the code to make the search for cities and locations
+                            name = option[_this.props.property];
+                        }
+
+                      return <option key={key} value={code}>{name}</option>
                       })
                     }
                 </FormControl>
